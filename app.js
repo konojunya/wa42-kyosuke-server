5cf858924dde10ff02e5bd0ba0194d898b2f3334
konojunya <junya03dance@gmail.com>
2018-09-10 22:22:08 +0900

    良い感じ

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const parseString = require('xml2js').parseString;
+
+// load dot env
+require('dotenv').config();
 
 app.get("/", (req, res) => {
   res.json({
