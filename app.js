fd5e26c290b9e701cdf5ff5aedb8c2772213fd4d
konojunya <junya03dance@gmail.com>
2018-09-12 15:16:03 +0900

    restaurantのAPIできた

--- app.js
+++ app.js
@@ -6,10 +6,49 @@ const parseString = require('xml2js').parseString;
 // load dot env
 require('dotenv').config();
 
-app.get("/", (req, res) => {
-  res.json({
-    "hello": "world"
-  })
-})
+const getRestaurant = async () => {
+  try {
+    const res = await axios.get("https://api.gnavi.co.jp/RestSearchAPI/v3/", {
+      params: {
+        keyid: process.env.API_KEY,
+        latitude: 34.702485,
+        longitude: 135.495951,
+        range: 5
+      }
+    })
+    return res.data.rest.map(r => ({
+      name: r.name,
+      name_kana: r.name_kana,
+      category: r.category,
+      url: r.url,
+      url_mobile: r.url_mobile,
+      image_url: r.image_url,
+      address: r.address,
+      tel: r.tel
+    }))
+  } catch(e) {
+    throw e
+  }
+}
 
-app.listen("3000")
+const getWeather = async () => {
+  try {
+    const res = await axios.get();
+    console.log(res.data)
+  } catch(e) {
+    throw e;
+  }
+}
+
+// app.get("/api/all", async  (req, res) => {
+//   try {
+//     const rests = await getRestaurant();
+//     res.status(200);
+//     res.json(rests);
+//   } catch(e) {
+//     res.status(e.response.status);
+//     res.json(e.response);
+//   }
+// })
+
+// app.listen("3000")
